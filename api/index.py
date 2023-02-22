f044cec51845f346cc8eab0c73ef09d23c39bec1
antoniosjnr <antonio.sjnr@gmail.com>
2023-02-22 20:45:36 -0300

    ''

--- api/index.py
+++ api/index.py
@@ -1,7 +1,25 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
+import json
+import nltk
+from nltk.chat.util import Chat, reflections
 
 app = Flask(__name__)
 
-@app.route('/webhook',methods=['POST'])
+
+@app.route('/webhook', methods=['POST'])
 def webhook():
-    return request.json
+    data = json.loads(request.json)
+    text = data['message']['text']
+
+    rules = {
+        'oi': ['Olá!', 'Oi!'],
+        'como você está?': ['Estou bem, obrigado!', 'Estou ótimo, e você?'],
+        'eu estou bem': ['Que bom ouvir isso!', 'Ótimo!'],
+        'o que você pode fazer?': ['Posso responder perguntas sobre nossos produtos e serviços.',
+                                   'Posso ajudá-lo com qualquer dúvida que você tiver sobre nossos produtos e serviços.'],
+        'adeus': ['Até mais!', 'Tchau!']
+    }
+
+    chatbot = Chat(rules, reflections)
+
+    return chatbot.respond(text)
