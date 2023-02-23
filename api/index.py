2289521c9bda3d313546e60c58c38ade353cee68
antoniosjnr <antonio.sjnr@gmail.com>
2023-02-23 11:24:51 -0300

    ''

--- api/index.py
+++ api/index.py
@@ -1,20 +1,22 @@
 from flask import Flask, request
-import json
-import nltk
 from nltk.chat.util import Chat, reflections
 import requests
+import os
+import pymongo
 
 app = Flask(__name__)
 
-BOT_TOKEN = "6180542426:AAFsMzPGAQShjGwwhqd-MLTXqYzIV44Fh2c"
+def getRules():
+    querySearch = {"token": os.environ['BOT_TOKEN']}
+    mongoCol = getMongoCollection()
+    rulesMongo = mongoCol.find(querySearch)
+    rules = list()
+    for x in rulesMongo:
+        rules.append([x['pergunta'], x['respostas']])
+    return rules
 
-pairs = [
-    ['meu nome é (.*)', ['Olá %1!']],
-    ['(oi|olá|ola|dai|eai)', ['E ai mano! !', 'Opa!', 'Olá!']],
-    ['(.*) seu nome?', ['Meu nome é AntonioTesteBot']],
-    ['(.*) que você faz?', ['Sou apenas um bot para testes!']],
-    ['(.*) te criou?', ['O Antônio está me ensinando a responder perguntas']]
-]
+
+pairs = getRules()
 
 chatbot = Chat(pairs, reflections)
 
@@ -23,6 +25,16 @@ def webhook():
     textoMensagem = request.json['message']['text'].lower()
     chatId = request.json['message']['chat']['id']
     resposta = chatbot.respond(textoMensagem)
-    urlCompleta = "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&parse_mode=html".format(BOT_TOKEN,chatId,resposta)
+    urlCompleta = "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&parse_mode=html".format(os.environ['BOT_TOKEN'],chatId,resposta)
     requests.post(urlCompleta)
     return 'ok'
+
+@app.route('/insertRule', methods=['POST'])
+def insertRule():
+    mongoCol = getMongoCollection()
+    mongoCol.insert_one(request.json)
+
+def getMongoCollection():
+    mongoClient = pymongo.MongoClient(os.environ['MONGODB_URI'])
+    mongoDatabase = mongoClient[os.environ['DATABASE_NAME']]
+    return mongoDatabase[os.environ['RULES_COLLECTION']]
