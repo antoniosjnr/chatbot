fe79d23b14158654cf52d9160eb743cebf623264
antoniosjnr <antonio.sjnr@gmail.com>
2023-02-22 21:40:12 -0300

    ''

--- api/index.py
+++ api/index.py
@@ -1,9 +1,24 @@
 from flask import Flask, request
 import json
+import nltk
+from nltk.chat.util import Chat, reflections
+import requests
 
 app = Flask(__name__)
 
+BOT_TOKEN = "6180542426:AAFsMzPGAQShjGwwhqd-MLTXqYzIV44Fh2c"
+URL_BASE = "https://api.telegram.org/bot"
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    return request.json['message']['text']
+    textoMensagem = request.json['message']['text'].lower()
+    chatId = request.json['message']['chat']['id']
+    rules = getRules()
+    chatbot = Chat(rules, reflections)
+    resposta = chatbot.respond(textoMensagem)
+    urlCompleta = "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&parse_mode=html".format(BOT_TOKEN,chatId,resposta)
+    requests.post(urlCompleta)
+
+def getRules():
+    with open('..\rules.json') as rulesFile:
+        return json.load(rulesFile)
