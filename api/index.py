3ce058f6612ea420b993cff1f8c89c4b5f8884ad
antoniosjnr <antonio.sjnr@gmail.com>
2023-02-22 21:58:07 -0300

    ''

--- api/index.py
+++ api/index.py
@@ -8,18 +8,20 @@ app = Flask(__name__)
 
 BOT_TOKEN = "6180542426:AAFsMzPGAQShjGwwhqd-MLTXqYzIV44Fh2c"
 
+rules = {
+        'oi': ['Olá!', 'Oi!'],
+        'como você está?': ['Estou bem, obrigado!', 'Estou ótimo, e você?'],
+        'eu estou bem': ['Que bom ouvir isso!', 'Ótimo!'],
+        'o que você pode fazer?': ['Posso responder perguntas sobre nossos produtos e serviços.',
+                                   'Posso ajudá-lo com qualquer dúvida que você tiver sobre nossos produtos e serviços.'],
+        'adeus': ['Até mais!', 'Tchau!']
+    }
+chatbot = Chat(rules, reflections)
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     textoMensagem = request.json['message']['text'].lower()
     chatId = request.json['message']['chat']['id']
-    rules = {
-        'oi': ['Olá!'],
-        'como você está?': ['Estou bem, obrigado!'],
-        'eu estou bem': ['Que bom ouvir isso!'],
-        'o que você pode fazer?': ['Posso responder perguntas sobre nossos produtos e serviços.'],
-        'adeus': ['Até mais!']
-    }
-    chatbot = Chat(rules, reflections)
     resposta = chatbot.respond(textoMensagem)
     urlCompleta = "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&parse_mode=html".format(BOT_TOKEN,chatId,resposta)
     requests.post(urlCompleta)
