9bcaccbf4fae6ebfc803c137391397a48eeb5b11
antoniosjnr <antonio.sjnr@gmail.com>
2023-02-22 21:09:17 -0300

    ''

--- api/index.py
+++ api/index.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.get_json()
-    parsed_json = json.loads(data)
+    parsed_json = str(data, encoding='utf-8')
 
     return parsed_json['message']['text']
